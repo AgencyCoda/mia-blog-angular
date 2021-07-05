ad095d17baea1b22030966c21e35807d53f1414d
Matias Camiletti <matias.camiletti@gmail.com>
2021-07-04 22:42:45 -0300

    Load config mia form.

--- projects/agencycoda/mia-blog-panel/src/lib/pages/mia-post-edit-page/mia-post-edit-page.component.ts
+++ projects/agencycoda/mia-blog-panel/src/lib/pages/mia-post-edit-page/mia-post-edit-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MiaField, MiaFormComponent, MiaFormConfig } from '@agencycoda/mia-form';
+import { MiaPost } from '@agencycoda/mia-blog';
 
 @Component({
   selector: 'mia-post-edit-page',
@@ -7,11 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiaPostEditPageComponent implements OnInit {
 
-  html = '';
+  @ViewChild('miaForm') miaForm!: MiaFormComponent;
+
+  config!: MiaFormConfig;
+  item: MiaPost = new MiaPost();
+  isLoading = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadConfig();
   }
 
+  onClickSave() {
+    this.miaForm.submit().subscribe(result => {
+      console.log('click save');
+      console.log(result);
+    });
+  }
+
+  loadConfig() {
+    this.config = new MiaFormConfig();
+    this.config.hasSubmit = false;
+    this.config.fields = [
+      { key: 'photo_featured', type: MiaField.TYPE_PHOTO, label: 'Featured Photo' },
+      { key: 'photo_featured_mobile', type: MiaField.TYPE_PHOTO, label: 'Featured Photo in mobile' },
+      { key: 'title', type: MiaField.TYPE_STRING, label: 'Title' },
+      { key: 'summary', type: MiaField.TYPE_TEXT, label: 'Summary' },
+      { key: 'content', type: MiaField.TYPE_HTML, label: 'Content', extra: { height: 400 } },
+    ];
+  }
 }
--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MiaBlogPanelModule } from 'projects/agencycoda/mia-blog-panel/src/public-api';
+import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './pages/add/add.component';
+import { MiaCoreModule } from '@agencycoda/mia-core';
+import { MiaAuthModule } from '@agencycoda/mia-auth';
+import { MiaFormModule } from '@agencycoda/mia-form';
 
 @NgModule({
   declarations: [
@@ -14,6 +19,11 @@ import { AddComponent } from './pages/add/add.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    QuillModule.forRoot(),
+    MiaCoreModule,
+    MiaAuthModule,
+    MiaFormModule,
     MiaBlogPanelModule
   ],
   providers: [],
