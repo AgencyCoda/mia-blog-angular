dec9fac450cf0f6da747180163dcc24bb352e328
Matias Camiletti <matias.camiletti@gmail.com>
2021-12-11 19:59:10 -0300

    Update publics.

--- projects/agencycoda/mia-blog-panel/src/public-api.ts
+++ projects/agencycoda/mia-blog-panel/src/public-api.ts
@@ -5,8 +5,23 @@
 /**
  * Components
  */
-
  export * from './lib/pages/mia-post-edit-page/mia-post-edit-page.component';
+
+ /**
+ * Pages
+ */
+ export * from './lib/pages/mia-creator-post-page/mia-creator-post-page.component';
+
+ /**
+ * Fields
+ */
+  export * from './lib/fields/related-field/related-field.component';
+
+ /**
+ * Modals
+ */
+  export * from './lib/modals/related-content-modal/related-content-modal.component';
+    
 
 export * from './lib/mia-blog-panel.service';
 export * from './lib/mia-blog-panel.component';
